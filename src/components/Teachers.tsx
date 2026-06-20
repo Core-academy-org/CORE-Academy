6330bdc986920d51503352c9df01a9ee68e48da0
Core-academy-org <jbahodir400@gmail.com>
2026-06-20 12:05:51 +0500

    Update Teachers.tsx

--- src/components/Teachers.tsx
+++ src/components/Teachers.tsx
@@ -11,9 +11,9 @@ const getTeachers = (t: (key: string) => string) => [
     tags: ["Founder", "Strategist"]
   },
   {
-    name: "Ulmasov Dustyorkhon",
-    role: t("teacher_dustyor_role"),
-    bio: t("teacher_dustyor_bio"),
+    name: "Asilbek To'xtaboyev",
+    role: t("teacher_Asilbek_role"),
+    bio: t("teacher_Asilbek_bio"),
     img: "https://api.dicebear.com/7.x/initials/svg?seed=Ulmasov+Dustyorkhon&backgroundColor=020617,1e293b,0d1527&textColor=f59e0b&fontSize=38&fontWeight=750",
     tags: ["Manager", "Success Lead"]
   },
